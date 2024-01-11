13a703b2147e20b0e43ceff860035b3c6b6e3e60
Panupong <Pai28212m@gmail.com>
2024-01-11 14:07:55 +0700

    feat:query with mongo (finished)

--- exercise-2.js
+++ exercise-2.js
@@ -1,1 +1,12 @@
 // Paste the complete MQL here
+
+//- ให้เขียน Query โดยมีรายละเอียดดังนี้
+//- หายอดขายทั้งหมดและจำนวนของ Pizza ในแต่ละ Size
+//- โดยที่ให้เรียงผลลัพธ์ตามยอดขายที่มากที่สุด
+
+
+db.pizzaOrders.aggregate([
+    { $group : {_id: "$size"  , totalPrice : {$sum : "$total_price" } , quantiy :{$sum : "quantity"}}},
+    { $sort : {totalPrice : -1}}
+    
+  ])
