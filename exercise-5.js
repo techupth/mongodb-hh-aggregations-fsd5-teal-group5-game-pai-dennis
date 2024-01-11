fcb4b300de80263f02c188f63638dbf14acf95ab
tanasri <tanawan95@gmail.com>
2024-01-11 21:01:34 +0700

    completed excercise

--- exercise-5.js
+++ exercise-5.js
@@ -1,1 +1,9 @@
 // Paste the complete MQL here
+db.pizzaOrders.aggregate([
+  {
+    $group: {
+      _id: { month: { $month: "$created_at" }, year: { $year: "$created_at" } },
+      total_sales: { $sum: "$total_price" },
+    },
+  },
+]);
